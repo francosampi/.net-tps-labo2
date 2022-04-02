99416b095291df9822e2157fb6e7dfbb7cfaa0b1
Franco Sampietro <frankaifuls@gmail.com>
2022-04-02 12:50:59 -0300

    primer commit

---
+++ TP1/Sampietro.Franco.2A.TP1/Sampietro.Franco.2A.TP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sampietro.Franco._2A.TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
