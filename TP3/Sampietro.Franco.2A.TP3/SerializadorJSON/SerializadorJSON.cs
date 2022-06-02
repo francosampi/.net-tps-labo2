1b921184365dd79321388804c2a90af56a7ecc31
Franco Sampietro <frankaifuls@gmail.com>
2022-06-02 16:40:43 -0300

    instalo paquete json

---
+++ TP3/Sampietro.Franco.2A.TP3/SerializadorJSON/SerializadorJSON.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JSON=System.Text.Json;
+using Excepciones;
+
+namespace SerializadorJSON
+{
+    public class SerializadorJSON
+    {
+        public static bool serializarJSON<T>(int tamano, string nombreArchivo)
+        {
+            T[] arreglo = new T[tamano];
+
+            try
+            {
+                string objJson = JSON.JsonSerializer.Serialize(arreglo);
+                return true;
+            }
+            catch (Exception)
+            {
+                throw new ArchivoException("No se pudo guardar el archivo");
+            }
+        }
+    }
+}
